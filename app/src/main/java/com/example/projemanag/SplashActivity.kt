ce7a1731a081decfefac14b4bd4474adca2f6201
daniel shelton <danielshelton90@gmail.com>
2021-09-21 14:58:11 -0700

    added IntroActivity

--- app/src/main/java/com/example/projemanag/SplashActivity.kt
+++ app/src/main/java/com/example/projemanag/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.example.projemanag
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -18,5 +20,10 @@ class SplashActivity : AppCompatActivity() {
 
         val typeFace : Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
         tv_app_name.typeface = typeFace
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 750)
     }
 }
