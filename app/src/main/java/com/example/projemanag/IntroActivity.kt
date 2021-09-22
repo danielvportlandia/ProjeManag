443e9c4ed58089aacc32e6dbae9ec42d7a6142d4
daniel shelton <danielshelton90@gmail.com>
2021-09-21 21:41:52 -0700

    created SignInActivity

--- app/src/main/java/com/example/projemanag/IntroActivity.kt
+++ app/src/main/java/com/example/projemanag/IntroActivity.kt
@@ -20,5 +20,8 @@ class IntroActivity : AppCompatActivity() {
         btn_sign_up_intro.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
+        btn_sign_in_intro.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
     }
 }
