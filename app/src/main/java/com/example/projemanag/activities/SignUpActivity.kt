c9b45223dd216788d7d93aedab2c5fc3225ec383
daniel shelton <danielshelton90@gmail.com>
2021-09-23 21:01:52 -0700

    sign up button listeners

--- app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
@@ -2,7 +2,9 @@ package com.example.projemanag.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.projemanag.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
@@ -29,5 +31,41 @@ class SignUpActivity : AppCompatActivity() {
         toolbar_sign_up_activity.setNavigationOnClickListener {
             onBackPressed()
         }
+        btn_sign_up.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        val name: String = et_name.text.toString().trim { it <= ' ' }
+        val email: String = et_email.text.toString().trim { it <= ' ' }
+        val password: String = et_password.text.toString()
+
+        if (validateForm(name, email, password)) {
+            Toast.makeText(this@SignUpActivity, "now we can register a new user", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun validateForm(name: String, email: String, password: String) : Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter an email")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            } else -> {
+                true
+            }
+        }
+    }
+
+    private fun showErrorSnackBar(s: String) {
+        // TODO:
     }
 }
