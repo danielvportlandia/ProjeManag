0eb714357f2813d9544b1436f5665a90d9cb6dda
daniel shelton <danielshelton90@gmail.com>
2021-09-23 21:53:38 -0700

    setting up firestore + User model

---
+++ app/src/main/java/com/example/projemanag/firebase/FirestoreClass.kt
@@ -0,0 +1,14 @@
+package com.example.projemanag.firebase
+
+import com.example.projemanag.activities.SignUpActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirestoreClass {
+
+    private val mFireStore = FirebaseFirestore.getInstance()
+
+    fun registerUser(activity: SignUpActivity, userInfo: User) {
+
+    }
+
+}
