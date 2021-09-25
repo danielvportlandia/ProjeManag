44cdb483d5830fdd4b26249b60df7872287369cf
daniel shelton <danielshelton90@gmail.com>
2021-09-24 19:56:05 -0700

    created MyProfile activity

--- app/src/main/java/com/example/projemanag/activities/MainActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/MainActivity.kt
@@ -66,7 +66,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
-            R.id.nav_my_profile -> Toast.makeText(this, "My Profile", Toast.LENGTH_SHORT).show()
+            R.id.nav_my_profile -> startActivity(Intent(this, MyProfileActivity::class.java))
             R.id.nav_sign_out -> {
                 FirebaseAuth.getInstance().signOut()
                 val intent = Intent(this, IntroActivity::class.java)
