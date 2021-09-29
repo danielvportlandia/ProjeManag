bd2d90efbc0d5e6a68b93a8279504fc36d22fc49
daniel shelton <danielshelton90@gmail.com>
2021-09-28 22:08:39 -0700

    created ItemMember

--- app/src/main/java/com/example/projemanag/activities/MembersActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/MembersActivity.kt
@@ -3,10 +3,33 @@ package com.example.projemanag.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.projemanag.R
+import com.example.projemanag.models.Board
+import com.example.projemanag.utils.Constants
+import kotlinx.android.synthetic.main.activity_members.*
 
 class MembersActivity : AppCompatActivity() {
+
+    private lateinit var mBoardDetails : Board
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_members)
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra<Board>(Constants.BOARD_DETAIL)!!
+        }
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar_members_activity)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = resources.getString(R.string.members)
+        }
+        toolbar_members_activity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
