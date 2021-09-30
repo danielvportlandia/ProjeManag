18b98ea47a64fa6744e49242d07db3681aba3e00
daniel shelton <danielshelton90@gmail.com>
2021-09-29 19:54:13 -0700

    only display assigned users if different than just the main assigner

--- app/src/main/java/com/example/projemanag/adapters/CardListItemsAdapter.kt
+++ app/src/main/java/com/example/projemanag/adapters/CardListItemsAdapter.kt
@@ -5,9 +5,12 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projemanag.R
+import com.example.projemanag.activities.TaskListActivity
 import com.example.projemanag.models.Card
+import com.example.projemanag.models.SelectedMembers
 import kotlinx.android.synthetic.main.item_card.view.*
 
 open class CardListItemsAdapter(
@@ -55,6 +58,42 @@ open class CardListItemsAdapter(
                 holder.itemView.view_label_color.visibility = View.GONE
             }
             holder.itemView.tv_card_name.text = model.name
+
+            if ((context as TaskListActivity).mAssignedMemberDetailList.size > 0) {
+                val selectedMembersList: ArrayList<SelectedMembers> = ArrayList()
+                for (i in context.mAssignedMemberDetailList.indices) {
+                    for (j in model.assignedTo) {
+                        if (context.mAssignedMemberDetailList[i].id == j) {
+                            val selectedMembers = SelectedMembers(
+                                context.mAssignedMemberDetailList[i].id,
+                                context.mAssignedMemberDetailList[i].image,
+                            )
+                            selectedMembersList.add(selectedMembers)
+                        }
+                    }
+                }
+
+                if (selectedMembersList.size > 0) {
+                    if (selectedMembersList.size == 1 && selectedMembersList[0].id == model.createdBy) {
+                        holder.itemView.rv_card_selected_members_list.visibility = View.GONE
+                    } else {
+                        holder.itemView.rv_card_selected_members_list.visibility = View.VISIBLE
+                        holder.itemView.rv_card_selected_members_list.layoutManager = GridLayoutManager(context, 4)
+                        val adapter = CardMemberListItemsAdapter(context, selectedMembersList, false)
+                        holder.itemView.rv_card_selected_members_list.adapter = adapter
+                        adapter.setOnClickListener(object: CardMemberListItemsAdapter.OnClickListener {
+                            override fun onClick() {
+                                if (onClickListener != null) {
+                                    onClickListener!!.onClick(position)
+                                }
+                            }
+                        })
+                    }
+                } else {
+                    holder.itemView.rv_card_selected_members_list.visibility = View.GONE
+                }
+            }
+
             holder.itemView.setOnClickListener {
                 if (onClickListener != null) {
                     onClickListener!!.onClick(position)
