6683ce3fae6a180418647ff25f772e72a8ce23dc
daniel shelton <danielshelton90@gmail.com>
2021-09-30 10:24:28 -0700

    notifications part 1

--- app/src/main/java/com/example/projemanag/activities/MembersActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/MembersActivity.kt
@@ -2,6 +2,7 @@ package com.example.projemanag.activities
 
 import android.app.Activity
 import android.app.Dialog
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -16,6 +17,15 @@ import com.example.projemanag.models.User
 import com.example.projemanag.utils.Constants
 import kotlinx.android.synthetic.main.activity_members.*
 import kotlinx.android.synthetic.main.dialog_search_member.*
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
 
 class MembersActivity : BaseActivity() {
 
@@ -107,6 +117,87 @@ class MembersActivity : BaseActivity() {
         mAssignedMembersList.add(user)
         anyChangesMade = true
         setupMembersList(mAssignedMembersList)
+        SendNotificationToUserAsyncTask(mBoardDetails.name, user.fcmToken).execute()
+    }
+
+    private inner class SendNotificationToUserAsyncTask(val boardName: String, val token: String) : AsyncTask<Any, Void, String>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showProgressDialog(resources.getString(R.string.please_wait))
+        }
+
+        override fun doInBackground(vararg p0: Any?): String {
+            var result : String
+            var connection : HttpURLConnection? = null
+            try {
+                val url = URL(Constants.FCM_BASE_URL)
+                connection = url.openConnection() as HttpURLConnection
+                connection.doOutput = true
+                connection.doInput = true
+                connection.instanceFollowRedirects = false
+                connection.requestMethod = "POST"
+
+                connection.setRequestProperty("Content-type", "application/json")
+                connection.setRequestProperty("charset", "utf-8")
+                connection.setRequestProperty("Accept", "application/json")
+
+                connection.setRequestProperty(Constants.FCM_AUTHORIZATION, "${Constants.FCM_KEY}=${Constants.FCM_SERVER_KEY}")
+
+                connection.useCaches = false
+
+                val wr = DataOutputStream(connection.outputStream)
+                val jsonRequest = JSONObject()
+                val dataObject = JSONObject()
+                dataObject.put(Constants.FCM_KEY_TITLE, "Assigned to the board: $boardName")
+                dataObject.put(Constants.FCM_KEY_MESSAGE, "You have been assigned to the new board by ${mAssignedMembersList[0].name}")
+                jsonRequest.put(Constants.FCM_KEY_DATA, dataObject)
+                jsonRequest.put(Constants.FCM_KEY_TO, token)
+                wr.writeBytes(jsonRequest.toString())
+                wr.flush()
+                wr.close()
+
+                val httpResult : Int = connection.responseCode
+
+                if (httpResult == HttpURLConnection.HTTP_OK) {
+                    val inputStream = connection.inputStream
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    val sb = StringBuilder()
+                    var line : String?
+                    try {
+                        while(reader.readLine().also {line=it} != null) {
+                            sb.append(line+"\n")
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } finally {
+                        try {
+                            inputStream.close()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                    result = sb.toString()
+                } else {
+                    result = connection.responseMessage
+                }
+
+            } catch (e: SocketTimeoutException) {
+                result = "Connection Timeout"
+            } catch (e: Exception) {
+                result = "Error : " + e.message
+                e.printStackTrace()
+            } finally {
+                connection?.disconnect()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            hideProgressDialog()
+        }
+
     }
 
 }
